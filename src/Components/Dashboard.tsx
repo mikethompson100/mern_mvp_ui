8fad2d0ce47c2c374060bcc1f3631cf5d6fb25ae
mikethompson100 <mikethompson100@hotmail.com>
2026-03-10 09:48:03 -0400

    Add count button for users.

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -38,43 +38,65 @@ function DashBoard() {
   }
   return (
     <>
-    <main>
-    <h1>Dashboard page</h1>
-      
-      <br />
-      {user.username}
-      <br />
-      <button
-        onClick={async () => {
-          localStorage.removeItem("token");
-          navigate("/");
-        }}
-      >
-        Logout
-      </button>
-      <br />
-      <br />
-      <br />
-      <button
-        onClick={async () => {
-          const token = localStorage.getItem("token");
-          if (!token) {
-            throw new Error("Token missing");
-          }
-          const url = "http://localhost:4000/users/";
-          const init = {
-            method: "DELETE",
-            headers: {
-              authorization: token,
-            },
-          };
-          await fetch(url, init);
-          localStorage.removeItem("token");
-          navigate("/");
-        }}
-      >
-        DELETE MY ACCOUNT
-      </button>
+      <main>
+        <h1>Dashboard page</h1>
+
+        <br />
+        {user.username}
+        <br />
+        <button
+          onClick={async () => {
+            localStorage.removeItem("token");
+            navigate("/");
+          }}
+        >
+          Logout
+        </button>
+        <br />
+        <br />
+        {user.count}
+        <button
+          id="count"
+          onClick={async () => {
+            const newCount = user.count + 1;
+            setUser({ ...user, count: newCount });
+            const url = "http://localhost:4000/users/count";
+            const init = {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ username: user.username, count: newCount })
+            };
+            const response = await fetch(url, init);
+            const data = await response.json();
+            console.log("DATA", data)
+          }}
+        >
+          Add 1 to counter
+        </button>
+
+        <br />
+        <button
+          onClick={async () => {
+            const token = localStorage.getItem("token");
+            if (!token) {
+              throw new Error("Token missing");
+            }
+            const url = "http://localhost:4000/users/";
+            const init = {
+              method: "DELETE",
+              headers: {
+                authorization: token,
+              },
+            };
+            await fetch(url, init);
+            localStorage.removeItem("token");
+            navigate("/");
+          }}
+        >
+          DELETE MY ACCOUNT
+        </button>
       </main>
     </>
   );
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -10,89 +10,92 @@ function Login() {
 
   return (
     <>
-    <main>
-    <h1>Login page</h1>
-      <div className="loginBox">
-        <form>
-          <label>
-            Username:
-            <input
-              id="username"
-              value={username}
-              onChange={(event) => {
-                setUsername(event.target.value);
+      <main>
+        <h1>Login page</h1>
+        <div className="loginBox">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <label>
+              Username:
+              <input
+                id="username"
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+                required
+              />
+            </label>
+            <br />
+            <button
+              onClick={async () => {
+                const url = "http://localhost:4000/users/login";
+                const init = {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ username, password }),
+                };
+                const response = await fetch(url, init);
+                const data = await response.json();
+
+                if (!response.ok) {
+                  setError(data.message);
+                  setPassword("");
+                  return;
+                }
+
+                localStorage.setItem("token", data.token);
+                navigate("/dashboard");
               }}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(event) => {
-                setPassword(event.target.value);
+            >
+              Login
+            </button>
+            <button
+              onClick={async () => {
+                const url = "http://localhost:4000/users";
+                const init = {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ username, password }),
+                };
+                const response = await fetch(url, init);
+                const data = await response.json();
+
+                if (!response.ok) {
+                  setError(data.message); // show backend message
+                  setUsername("");
+                  setPassword("");
+                  return;
+                }
+                localStorage.setItem("token", data.token);
+                navigate("/dashboard");
               }}
-              required
-            />
-          </label>
-        </form>
-        <br />
-        <button
-          onClick={async () => {
-            const url = "http://localhost:4000/users/login";
-            const init = {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ username, password }),
-            };
-            const response = await fetch(url, init);
-            const data = await response.json();
-
-            if (!response.ok) {
-              setError(data.message);
-              setPassword("");
-              return;
-            }
-
-            localStorage.setItem("token", data.token);
-            navigate("/dashboard");
-          }}
-        >
-          Login
-        </button>
-        <button
-          onClick={async () => {
-            const url = "http://localhost:4000/users";
-            const init = {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ username, password }),
-            };
-            const response = await fetch(url, init);
-            const data = await response.json();
-
-            if (!response.ok) {
-              setError(data.message); // show backend message
-              setUsername("");
-              setPassword("");
-              return;
-            }
-
-            localStorage.setItem("token", data.token);
-            navigate("/dashboard");
-          }}
-        >
-          Register
-        </button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </div>
+            >
+              Register
+            </button>
+          </form>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
       </main>
     </>
   );
